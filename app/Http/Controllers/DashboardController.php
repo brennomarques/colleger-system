1261af924fcecff6820c895205ae6a96b7924efa
brenno.lima <brenno.lima@soluti.com.br>
2021-12-29 18:19:28 -0300

    Melhorado dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\{Plan, Student, teacher};
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
